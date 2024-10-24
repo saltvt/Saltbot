dfa533f1002cfeed4dad90487c554b87755f3d8a
saltvt <ggsaltvt@gmail.com>
2024-10-24 06:41:01 -0400

    Refactor and documentation update.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Saltbotv1
+# Saltbotv2 
 Chatbot 
-The goal of this project is to create my own LLM/NN for stream purposes and to document the experience.
-Use of this repository is free. Do not redistribute or pay money for any of the code in this repository.
+The goal of this project is to create my own LLM/NN for stream purposes and to document the experience. First versions will be using chatterbot and then the subsequent versions will be using Pytorch. Tensorflow IS NOT IT. XD. This first version should be fully functional even though it's simple. Will ad more when I get more comfortable with the data and math.
 
-Setting up proper file structure and testing locally, sorry XD
+Use of this repository is free. Do not redistribute or pay money for any of the code in this repository, I want this to be free of use so the only need is compute.
+-Salt
---
+++ aiV2.py
@@ -0,0 +1,31 @@
+from chatterbot import Chatbot
+from chatterbot.trainers import ChatterbotCorpusTrainer
+
+
+saltbot = ChatBot(
+    'saltbot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite'
+)
+trainer = ChatterBotCorpusTrainer(saltbot)
+trainer.train(
+    'chatterbot.corpus.english'
+)
+
+while True:
+    try:
+        user_input = input('You: ')
+        bot_response = saltbot.get_response(user_input)
+        print('Bot:', bot_response)
+        
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
+    
+    
+#initializing the bot and run it with the chatterbot dependency. Needs more research. Add Functionality for twitch and stand alone Application(server side)
+
+#training data needed (specifically for Vtuber culture and Twitch Culture)
+
+#dependencies Python3.7./Python3.8/Conda
+
+#Work on UI 
